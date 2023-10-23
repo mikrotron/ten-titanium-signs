0902e9308723e1071d34139e5c7df73bb6812aa5
Mike Polowick <mikrotron@users.noreply.github.com>
2023-10-23 07:09:41 +0000

    Remove console.log

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,7 +4,6 @@ export const pollingInterval = 1000;
 export const newCommentDelay = 2500;
 
 export const server = window.location.origin.replace("5173", "3001");
-console.log("HOLLA", server);
 export const endpoint = {
   fetch: `${server}/getComments`,
   create: `${server}/createComment`,
